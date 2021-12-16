64fc6207ca7e53c8c0d17aa4eb502bae3a18fe06
Dotni <57830936+Dotni@users.noreply.github.com>
2021-12-16 16:13:16 +0100

    User technology controller done

---
+++ Application/UseCases/UserTechnology/Get/UseCaseGetUserTechnologyByIdUser.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using Application.UseCases.DeveloperProject.Dtos;
+using Application.UseCases.UserTechnology.Dtos;
+using Application.UseCases.Utils;
+using Infrastructure.SqlServer.Repositories.UserTechnology;
+
+namespace Application.UseCases.UserTechnology.Get
+{
+    public class UseCaseGetUserTechnologyByIdUser : IQueryFiltering<List<OutputDtoUserTechnology>,int>
+    {
+        private readonly IUserTechnologyRepository _userTechnologyRepository;
+
+        public UseCaseGetUserTechnologyByIdUser(IUserTechnologyRepository userTechnologyRepository)
+        {
+            _userTechnologyRepository = userTechnologyRepository;
+        }
+
+
+        public List<OutputDtoUserTechnology> Execute(int filter)
+        {
+            var userTechnology = _userTechnologyRepository.GetByUserId(filter);
+            
+            return Mapper.GetInstance().Map<List<OutputDtoUserTechnology>>(userTechnology);
+        }
+    }
+}
---
+++ WebAPI/Controllers/UserTechnologyController.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections.Generic;
+using Application.UseCases.UserTechnology.Delete;
+using Application.UseCases.UserTechnology.Dtos;
+using Application.UseCases.UserTechnology.Get;
+using Application.UseCases.UserTechnology.Post;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace WebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/userTechnologies")]
+    public class UserTechnologyController : ControllerBase
+    {
+        // Use cases
+        private readonly UseCaseGetAllUserTechnologies _useCaseGetAllUserTechnologies;
+        private readonly UseCaseGetUserTechnologyByIdTechnology _useCaseGetUserTechnologyByIdTechnology;
+        private readonly UseCaseGetUserTechnologyByIdUser _useCaseGetUserTechnologyByIdUser;
+        private readonly UseCaseCreateUserTechnology _useCaseCreateUserTechnology;
+        private readonly UseCaseDeleteUserTechnology _useCaseDeleteUserTechnology;
+
+        public UserTechnologyController(
+            UseCaseGetAllUserTechnologies useCaseGetAllUserTechnologies,
+            UseCaseGetUserTechnologyByIdTechnology useCaseGetUserTechnologyByIdTechnology,
+            UseCaseGetUserTechnologyByIdUser useCaseGetUserTechnologyByIdUser,
+            UseCaseCreateUserTechnology useCaseCreateUserTechnology,
+            UseCaseDeleteUserTechnology useCaseDeleteUserTechnology)
+        {
+            _useCaseGetAllUserTechnologies = useCaseGetAllUserTechnologies;
+            _useCaseGetUserTechnologyByIdTechnology = useCaseGetUserTechnologyByIdTechnology;
+            _useCaseGetUserTechnologyByIdUser = useCaseGetUserTechnologyByIdUser;
+            _useCaseCreateUserTechnology = useCaseCreateUserTechnology;
+            _useCaseDeleteUserTechnology = useCaseDeleteUserTechnology;
+        }
+        
+        // Get requests
+        [HttpGet]
+        public ActionResult<List<OutputDtoUserTechnology>> GetAll()
+        {
+            return _useCaseGetAllUserTechnologies.Execute();
+        }
+        
+        [HttpGet]
+        [Route("byUser/{idUser:int}")]
+        public ActionResult<List<OutputDtoUserTechnology>> GetByIdUser(int idUser)
+        {
+            return _useCaseGetUserTechnologyByIdUser.Execute(idUser);
+        }
+        
+        [HttpGet]
+        [Route("byTechnology/{idTechnology:int}")]
+        public ActionResult<List<OutputDtoUserTechnology>> GetByIdTechnology(int idTechnology)
+        {
+            return _useCaseGetUserTechnologyByIdTechnology.Execute(idTechnology);
+        }
+        
+        // Post requests
+        [HttpPost]
+        [ProducesResponseType(201)]
+        [ProducesResponseType(400)]
+        public ActionResult<OutputDtoUserTechnology> Create([FromBody] InputDtoUserTechnology inputDtoUserTechnology)
+        {
+            return StatusCode(201, _useCaseCreateUserTechnology.Execute(inputDtoUserTechnology));
+        }
+        
+        // Delete requests
+        [HttpDelete]
+        [Route("{idUser:int},{idTechnology:int}")]
+        public ActionResult Delete(int idUser, int idTechnology)
+        {
+            var result = _useCaseDeleteUserTechnology.Execute(idUser,idTechnology);
+            
+            if (result) return Ok();
+            return NotFound();
+        }
+        
+    }
+}
