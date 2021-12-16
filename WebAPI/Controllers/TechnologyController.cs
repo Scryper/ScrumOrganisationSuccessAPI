665ea74b5727daccfb30bbb78c4c806fa3549108
Dotni <57830936+Dotni@users.noreply.github.com>
2021-12-16 13:13:56 +0100

    technology Controller done

--- WebAPI/Controllers/TechnologyController.cs
+++ WebAPI/Controllers/TechnologyController.cs
@@ -1,4 +1,6 @@
-﻿using Application.UseCases.Technology.Get;
+﻿using System.Collections.Generic;
+using Application.UseCases.Technology.Dtos;
+using Application.UseCases.Technology.Get;
 using Microsoft.AspNetCore.Mvc;
 
 namespace WebAPI.Controllers
@@ -9,5 +11,38 @@ namespace WebAPI.Controllers
     {
         // Use cases 
         private readonly UseCaseGetAllTechnologies _useCaseGetAllTechnologies;
+        private readonly UseCaseGetTechnologyById _useCaseGetTechnologyById;
+        private readonly UseCaseGetTechnologyByName _useCaseGetTechnologyByName;
+
+        public TechnologyController(
+            UseCaseGetAllTechnologies useCaseGetAllTechnologies,
+            UseCaseGetTechnologyById useCaseGetTechnologyById,
+            UseCaseGetTechnologyByName useCaseGetTechnologyByName)
+        {
+            _useCaseGetAllTechnologies = useCaseGetAllTechnologies;
+            _useCaseGetTechnologyById = useCaseGetTechnologyById;
+            _useCaseGetTechnologyByName = useCaseGetTechnologyByName;
+        }
+        
+        // Get requests
+        [HttpGet]
+        public ActionResult<List<OutputDtoTechnology>> GetAll()
+        {
+            return _useCaseGetAllTechnologies.Execute();
+        }
+
+        [HttpGet]
+        [Route("byId/{id:int}")] 
+        public ActionResult<OutputDtoTechnology> GetById(int id)
+        {
+            return _useCaseGetTechnologyById.Execute(id);
+        }
+        
+        [HttpGet]
+        [Route("byName/{name:alpha}")] 
+        public ActionResult<OutputDtoTechnology> GetByName(string name)
+        {
+            return _useCaseGetTechnologyByName.Execute(name);
+        }
     }
 }
