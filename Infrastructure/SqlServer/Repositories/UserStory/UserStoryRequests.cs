853fcab50e2b5a22028cae1be80f5632801e8eb1
Scryper <florian.mazzeo@gmail.com>
2021-12-18 18:40:24 +0100

    changes for user story repo because of db organisation changes

--- Infrastructure/SqlServer/Repositories/UserStory/UserStoryRequests.cs
+++ Infrastructure/SqlServer/Repositories/UserStory/UserStoryRequests.cs
@@ -3,8 +3,6 @@
     public partial class UserStoryRepository
     {
         public const string TableName = "user_story";
-        public const string SprintUserStoryTableName = "sprint_user_story";
-        public const string SprintTableName = "sprint";
         
         public const string ColId = "id";
         public const string ColName = "name";
@@ -12,22 +10,15 @@
         public const string ColPriority = "priority";
         public const string ColIdProject = "id_project";
         
-        public const string UserStoryColIdProject = "user_story.id_project";
-        public const string SprintUserColIdUserStory = "sprint_user_story.id_user_story";
-        public const string SprintUserColIdSprint = "sprint_user_story.id_sprint";
-        public const string SprintColId = "sprint.id";
-        
         // Get requests
         private static readonly string ReqGetAll = $"select * from {TableName}";
 
         private static readonly string ReqGetById = $@"select * from {TableName} 
-                                                    where {ColId} = @{ColId}"; 
+                                                    where {ColId} = @{ColId}";
         
         private static readonly string ReqGetByIdProject = 
                                     $@"select * from {TableName} 
-                                    left join {SprintUserStoryTableName} on {ColId} = {SprintUserColIdUserStory} 
-                                    left join {SprintTableName} on {SprintUserColIdSprint} = {SprintColId} 
-                                    where {UserStoryColIdProject} = @{ColId}";
+                                    where {ColIdProject} = @{ColIdProject}";
         
         // Post requests
         private static readonly string ReqCreate = 
