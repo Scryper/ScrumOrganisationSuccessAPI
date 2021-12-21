124f49ed0712e328e4874bac0fc26f45fd98ffb8
Scryper <florian.mazzeo@gmail.com>
2021-12-21 20:09:38 +0100

    add method to get the number of sprints in a project, add unit test

---
+++ Application/Services/Sprint/SprintService.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Infrastructure.SqlServer.Repositories.Sprint;
+
+namespace Application.Services.Sprint
+{
+    public class SprintService : ISprintService
+    {
+        private readonly ISprintRepository _sprintRepository;
+
+        public SprintService(ISprintRepository sprintRepository)
+        {
+            _sprintRepository = sprintRepository;
+        }
+        
+        public List<Domain.Sprint> GetByIdProject(int idProject)
+        {
+            return _sprintRepository.GetByIdProject(idProject);
+        }
+
+        public int FindMaxSprintNumber(List<Domain.Sprint> sprints)
+        {
+            var sprintNumbers = new int[sprints.Count];
+            for (int i = 0 ; i < sprints.Count ; i++)
+            {
+                sprintNumbers.SetValue(sprints[i].SprintNumber, i);
+            }
+
+            return sprintNumbers.Max();
+        }
+    }
+}
